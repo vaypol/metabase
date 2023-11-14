48cbeb9cc94184180eb2b2d49fe8e10dddb92677
Emmad Usmani <emmadusmani@berkeley.edu>
2023-11-14 09:24:49 -0800

    DO NOT MERGE emmad combo chart debugging

--- frontend/src/metabase/visualizations/echarts/cartesian/model/series.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/model/series.ts
@@ -109,8 +109,9 @@ export const getCardSeriesModels = (
         false,
         card.name,
       );
-      const legacySeriesSettingsObjectKey =
-        createLegacySeriesObjectKey(vizSettingsKey);
+      const legacySeriesSettingsObjectKey = createLegacySeriesObjectKey(
+        metric.column.name, // <- changed this from vizSettingsKey for debugging, do not merge to master
+      );
 
       const name =
         settings[SERIES_SETTING_KEY]?.[vizSettingsKey]?.title ??
@@ -156,8 +157,9 @@ export const getCardSeriesModels = (
       true,
       card.name,
     );
-    const legacySeriesSettingsObjectKey =
-      createLegacySeriesObjectKey(vizSettingsKey);
+    const legacySeriesSettingsObjectKey = createLegacySeriesObjectKey(
+      metric.column.name,
+    );
 
     const name =
       settings.series_settings?.[vizSettingsKey]?.title ??
