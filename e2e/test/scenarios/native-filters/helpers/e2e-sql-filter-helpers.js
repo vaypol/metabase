6f89e18b6e66671dae6e07dd6ea22aac30ae50c1
Ryan Laurie <iethree@gmail.com>
2023-11-27 16:16:47 -0700

    fix more sql typing

--- e2e/test/scenarios/native-filters/helpers/e2e-sql-filter-helpers.js
+++ e2e/test/scenarios/native-filters/helpers/e2e-sql-filter-helpers.js
@@ -84,8 +84,10 @@ export function runQuery(xhrAlias = "dataset") {
  * @param {string} query
  */
 export function enterParameterizedQuery(query, options = {}) {
-  cy.get("@editor").type(query, {
-    parseSpecialCharSequences: false,
-    ...options,
-  });
+  cy.get("@editor")
+    .click()
+    .type(query, {
+      parseSpecialCharSequences: false,
+      ...options,
+    });
 }
